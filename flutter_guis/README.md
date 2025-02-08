651b9898f45597053e53b20cf977689a8438bf02
Rohan Akki <roha.akki@gmail.com>
2025-02-08 17:58:24 -0500

    freddy random popup exe

---
+++ flutter_guis/README.md
@@ -0,0 +1,3 @@
+# flutter_guis
+
+A new Flutter project.
---
+++ flutter_guis/windows/runner/main.cpp
@@ -0,0 +1,53 @@
+#include <flutter/dart_project.h>
+#include <flutter/flutter_view_controller.h>
+#include <windows.h>
+
+#include "flutter_window.h"
+#include "utils.h"
+#include <random>
+
+int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
+                      _In_ wchar_t *command_line, _In_ int show_command) {
+  // Attach to console when present (e.g., 'flutter run') or create a
+  // new console when running with a debugger.
+  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+    CreateAndAttachConsole();
+  }
+
+  // Initialize COM, so that it is available for use in the library and/or
+  // plugins.
+  ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
+  flutter::DartProject project(L"data");
+
+  std::vector<std::string> command_line_arguments =
+      GetCommandLineArguments();
+
+  project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
+
+  FlutterWindow window(project);
+  
+  std::random_device rd;
+  std::mt19937 generator(rd()); 
+
+  // Define a distribution
+  std::uniform_int_distribution<int> distributionX(0, (int) GetSystemMetrics(SM_CXSCREEN)/2);
+  std::uniform_int_distribution<int> distributionY(0, (int) GetSystemMetrics(SM_CYSCREEN)/2);
+
+
+  Win32Window::Point origin(distributionX(generator), distributionY(generator));
+  Win32Window::Size size(900, 900);
+  if (!window.Create(L"Har Har HarHar Har...", origin, size)) {
+    return EXIT_FAILURE;
+  }
+  window.SetQuitOnClose(true);
+
+  ::MSG msg;
+  while (::GetMessage(&msg, nullptr, 0, 0)) {
+    ::TranslateMessage(&msg);
+    ::DispatchMessage(&msg);
+  }
+
+  ::CoUninitialize();
+  return EXIT_SUCCESS;
+}
